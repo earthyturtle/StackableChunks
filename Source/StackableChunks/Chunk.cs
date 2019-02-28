a580b7b41796ecbd1a1dda8849ccc53f6bac278f
TW <temple_wing@163.com>
2019-02-28 21:25:35 +0800

    bug fix

--- Source/StackableChunks/Chunk.cs
+++ Source/StackableChunks/Chunk.cs
@@ -4,8 +4,8 @@ using Verse;
 namespace StackableChunks {
     public class Chunk : Thing {
         public override void ExposeData() {
-            base.ExposeData();
             Scribe_Values.Look<int>(ref this.stackCount, "stackCount", 0, true);
+            base.ExposeData();
         }
     }
 }
