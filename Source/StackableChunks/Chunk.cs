35f08c2954721b7ffbd70f0e286e97ed1de886e3
TW <temple_wing@163.com>
2019-02-28 18:51:35 +0800

    save stackCount revert last stacklimit change

---
+++ Source/StackableChunks/Chunk.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Verse;
+
+namespace StackableChunks {
+    public class Chunk : Thing {
+        public override void ExposeData() {
+            base.ExposeData();
+            Scribe_Values.Look<int>(ref this.stackCount, "stackCount", 0, true);
+        }
+    }
+}
